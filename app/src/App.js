dc2aa111ec97402d9730dff1c88b0915416bd305
erica-y-chen <eyufanchen@gmail.com>
2019-05-14 13:09:41 -0700

    Added 4 buttons for dashboard

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h3>hello world!</h3>
+        <button>strike</button>
+        <button>ball</button>
+        <button>foul</button>
+        <button>hit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// ### Count Rules
+
+// - balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+// - balls and strikes reset to 0 when a `hit` is recorded.
+// - a `foul` increases strikes up to 2. With no strikes, a foul makes it 1 strike. With 1 strike, a foul makes it 2 strikes. With two strikes a foul has no effect, count stays at 2 strikes.
+
+// ### Display
+
+// - display the count of `balls` and `strikes` for the at-bat.
+// - should be updated when the user records activity on the `Dashboard` component.
+
+// ### Dashboard
+
+// - provide a button that the person in charge can press every time there is a `strike`, `ball`, `foul` or `hit`.
+// - there is **NO** need to specify the type of hit (single, double, etc).
+// - changes recorded on this component should update the information shown by the `Display` component.
--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+
+  it('should display hello word', () => {
+    const { getByText } = render(<App />);
+    getByText(/hello world!/i);
+
+  })
+})
